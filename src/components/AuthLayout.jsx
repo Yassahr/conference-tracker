0eef81f2dbea03455aac9968d498cf87bdadb31f
Yassahr <yassahr@gmail.com>
2025-06-25 18:46:59 -0400

    added pages and auth validation

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+
+
 //this is for auth, to make sure you are logged in to the correct place to login into a page
+//checking if the user is logged in usinf store to check the state
 function Protected ({children, authentication = true}){
-    return (
-        <>
-            {children}
-        </>
-    );
+    const authStatus = useSelector((state =>{
+         return state.auth.status
+    }))
+
+    const navigate = useNavigate()
+    const [loader, setLoader] = useState(true)
+
+    useEffect(()=>{
+        //if auth is need but your authStatus(if you are logged in) is false
+        if(authentication && authStatus !== authentication){
+            navigate("/login")
+        //if auth is not needed but the user is logged in=> go to home page
+        }else if(!authentication && authStatus !== authentication){
+            navigate("/")
+        }
+        setLoader(false)
+    },[authStatus, authentication, navigate ])
+
+//replace null with the loader
+    return loader ? null : <>{children}</>
 };
 
 export default Protected;
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,26 @@
-// import { StrictMode } from 'react'
+import {  StrictMode } from 'react'
+import React from 'react'
 // import { createRoot } from 'react-dom/client'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createbrowserRouter, RouterProvider } from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { Provider} from 'react-redux'
 import store from './store/store.js'
 import Home from "./pages/Home.jsx"
 import Login from "./pages/Login.jsx"
-import AuthLayout from  "./components/AuthLayout.jsx"
+import Protected from  "./components/AuthLayout.jsx"
 import SignUp from "./pages/SignUp.jsx"
+import AllPosts from "./pages/AllPosts.jsx"
+import AddPost from "./pages/AddPost.jsx"
+import EditPost from "./pages/EditPost.jsx"
+import Post from "./pages/Post.jsx"
 
 
 
-const router = createbrowserRouter([
+
+
+const router = createBrowserRouter([
   {
     path: "/",
     element: <App/>,
@@ -26,18 +33,49 @@ const router = createbrowserRouter([
         path: "/login",
         element: (
           // uses prop to confirm whether or not auth is needed at this page
-          <AuthLayout authentication = {false}>
+          <Protected authentication = {false}>
             < Login />
-          </AuthLayout>
+          </Protected>
         )
       },
       {
         path: "/signup",
         element: (
-          // uses prop to confirm whether or not auth is needed at this page
-          <AuthLayout authentication = {false}>
+          <Protected authentication = {false}>
             < SignUp />
-          </AuthLayout>
+          </Protected>
+        )
+      },
+      {
+        path: "/all-posts",
+        element: (
+          <Protected authentication>
+            < AllPosts />
+          </Protected>
+        )
+      },
+      {
+        path: "/add-post",
+        element: (
+          <Protected authentication>
+            < AddPost />
+          </Protected>
+        )
+      },
+      {
+        path: "/edit-post/:slug",
+        element: (
+          <Protected authentication>
+            < EditPost />
+          </Protected>
+        )
+      },
+      {
+        path: "/post/:slug",
+        element: (
+          <Protected authentication>
+            < Post />
+          </Protected>
         )
       },
     ]
