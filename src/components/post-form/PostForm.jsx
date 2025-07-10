2230df034edfe45536437224435b35a21925c4be
Yassahr <yassahr@gmail.com>
2025-07-09 23:07:32 -0400

    built out post-form

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -8,23 +8,47 @@ import appwriteService from "../../appwrite/config"
 import { useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
 
+//**interdependence on the useForm, handleSubmit, and onSubmit attribute on form com
 export default function PostForm({post}){
     const {register, handleSubmit, watch, setValue, control,  getValues}= useForm({
         defaultValues:{
             title: post?.title|| "",
             slug: post?.slug|| "",
             content: slug?.content|| "",
-            active: post?.content|| "active",
-
-            
-
-
+            active: post?.content|| "active"
         }
     })
     const navigate = useNavigate()
     //useSelector gives you information about user state
     const userData = useSelector((state)=> state.auth.userData)
-     const submit= async(data)=> {}
+     const submit= async(data)=> {
+        if(post){
+            const file = data.image? await appwriteService.uploadFile(data.image[0]):null
+
+            if(file){
+                await appwriteService.deleteFile(post.featuredImage)
+            }
+            const dbPost = await appwriteService.updatePost(post.$id, {
+                ...data,
+                featuredImage: file ? file.$id : undefined
+            })
+            if(dbPost){
+                navigate(`/post/${dbPost.$id}`)
+            }
+        } else {
+            const file = await appwriteService.uploadFile(data.image[0])
+
+            if(file){
+                const fileId= file.$id
+                data.featuredImage = fileId
+                const dbpost= await appwriteService.createPost({...data, userID: userData.$id})
+
+                if(dbPost){
+                    navigate(`/post/${dbPost.$id}`)
+                }
+            }
+        }
+     }
     
      const transformSlug = useCallback((value)=>{
         if(value && typeof value === "string")
@@ -39,6 +63,7 @@ export default function PostForm({post}){
      React.useEffect(()=>{
 //value inside of watch is all of the values that are inside of the form
 //somehow react just knows that this watch is for the useForm(hover over values to see all the associated props)
+//watch simply returns values-> on submit actually store then  and saves slug
         watch((value, {name})=>{
             if(name==="title"){
                 setValue("slug", transformSlug(value, title))
@@ -54,9 +79,53 @@ return(
             placeholder="Title"
             className="mb-4"
             {...register("title")}/>
+            <Input
+            label="Slug :"
+            placeholder="Slug"
+            className="mb-4"
+            //register is giving the values to the input
+            {...register("slug", {required:true})}
+            onInput= {(e)=>{
+                setValue("slug", transformSlug(e.currentTarget.value), {shouldValidate: true})
+
+            }}
+            //this is actually takes values into form 
+            />
+            <RTE 
+            label="Content: "
+            name="content"
+            control={control}
+            dafaultValue={getValues("content")}/>
+
 
         </div>
-
+        <div classname="1/3 px-2">
+            <Input 
+            label="Featured Image"
+            type="file"
+            className="mb-4"
+            accept="images/png, image/jpeg, image/jpg"
+            {...register("image", {required: !post})}
+            />
+            {post && (
+                <div className="w-full mb-4">
+                    <img src={appwriteService.getFilePreview(post.featuredImage)}
+                    alt={post.title}
+                    className="rounded-lg"/>
+                </div>
+            )}
+            <Select
+            options={["active", "inactive"]}
+            label="Status"
+            className="mb-4"
+            {...register("status", {required: true})}/>
+            <Button 
+            type="submit"
+            bgColor={post? "bg-green-500": undefined}
+            className="w-full">
+                {post ? "Update": "Submit"}
+            </Button>
+        </div>
     </form>
 )
 
